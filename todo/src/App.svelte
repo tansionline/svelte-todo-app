e42de5b7fa8a72b725e2c52749aa66392a791011
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-12-03 16:39:15 +0300

    style done

--- todo/src/App.svelte
+++ todo/src/App.svelte
@@ -28,34 +28,84 @@
 </script>
 
 <style>
+  /* Google Fonts */
+  @import url("https://fonts.googleapis.com/css2?family=Nerko+One&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Sansita+Swashed&display=swap");
+
   main {
     text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+    max-width: none;
   }
 
-  @media (min-width: 640px) {
+  @media (max-width: 640px) {
     main {
       max-width: none;
     }
   }
-  h1 {
-    color: rgb(32, 168, 123);
+
+  .color-1 {
+    color: rgb(27, 26, 26);
+    font-family: "Nerko One", cursive;
+    font-size: 35px;
+  }
+
+  button.color-2 {
+    color: rgb(23, 31, 31);
+    background-color: rgb(8, 134, 82);
+    font-size: 25px;
+  }
+
+  button.color-2:active {
+    background-color: black;
+  }
+
+  .todo {
+    display: grid;
+  }
+
+  .todos-input {
+    padding: 1% 1%;
+    width: 80%;
+  }
+
+  .todos-input:focus {
+    background-color: rgb(8, 134, 82);
+  }
+
+  .todo-button {
+    background-color: rgb(8, 134, 82);
+    width: 10%;
+    padding: 1%;
+    font-family: "Sansita Swashed", cursive;
   }
 </style>
 
+<link
+  rel="stylesheet"
+  href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+  integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+  crossorigin="anonymous" />
 <main>
   <div>
     <input
       type="text"
-      class="todos"
+      class="todos-input"
       placeholder="Your Todo"
       bind:value={todos} />
-    <button class="ToDo-Add" on:click={addTodo}>Add TODO</button>
+
+    <button class="todo-button" on:click={addTodo}>Add</button>
   </div>
-  {#each list as item, i (item)}
-    <h1>{item.text}</h1>
-  {/each}
-  <div />
+
+  <div class="todo">
+    {#each list as item, i (item)}
+      <h1 class="color-1">{item.text}</h1>
+
+      <button class="color-2"><i class="fas fa-trash" /></button>
+    {/each}
+
+    <div />
+  </div>
 </main>
